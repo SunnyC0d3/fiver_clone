05b1fe4cac2cc905d97fa50a92f4f70136f69765
Sukhwinder <singhdevelops@gmail.com>
2024-11-21 13:34:11 +0000

    adding basic components and reducers

--- client/src/pages/_app.jsx
+++ client/src/pages/_app.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import '../global.css';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
 
-function _app() {
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useCookies } from 'react-cookie';
+import { useEffect } from 'react';
+import { StateProvider } from '../context/StateContext';
+import reducer, { initialState } from '../context/StateReducers';
+
+export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  const [cookies] = useCookies();
+  useEffect(() => {
+    if (
+      router.pathname.includes('/seller') ||
+      router.pathname.includes('/buyer')
+    ) {
+      if (!cookies.jwt) {
+        router.push('/');
+      }
+    }
+  }, [cookies, router]);
+
   return (
-    <div>_app</div>
-  )
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <title>Fiverr Clone</title>
+      </Head>
+      <div className="relative flex flex-col h-screen justify-between">
+        <Navbar />
+        <div
+          className={`${router.pathname !== '/' ? 'mt-36' : ''
+            } mb-auto w-full mx-auto`}
+        >
+          <Component {...pageProps} />
+        </div>
+        <Footer />
+      </div>
+    </StateProvider>
+  );
 }
-
-export default _app
